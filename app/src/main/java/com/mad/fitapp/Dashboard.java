672a1df7861431cebf8bd55ba7afb2f73984175b
SenuraJayadeva <senurajayadeva@gmail.com>
2020-10-03 18:10:26 +0530

    implemented signout and fixed user registration

--- app/src/main/java/com/mad/fitapp/Dashboard.java
+++ app/src/main/java/com/mad/fitapp/Dashboard.java
@@ -1,5 +1,7 @@
 package com.mad.fitapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +11,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Dashboard extends Fragment {
 
@@ -21,6 +25,19 @@ public class Dashboard extends Fragment {
         Button setGoalButtonFragment =  (Button) view.findViewById(R.id.setGoalButton);
         Button viewgoaloverviewButtonFragment =  (Button) view.findViewById(R.id.view_user_goal_btn);
         Button Set_daily_progress_fragment_btn =  (Button) view.findViewById(R.id.set_daily_progress_fragment_btn);
+        Button set_signout_button = (Button)view.findViewById(R.id.signoutbutton);
+
+        //Navigate button for set daily goal button
+        set_signout_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                FirebaseAuth.getInstance().signOut();
+                Intent i = new Intent(getActivity(), MainActivity.class);
+                startActivity(i);
+                ((Activity) getActivity()).overridePendingTransition(0, 0);
+            }
+
+        });
 
         //Navigate button for set daily goal button
         setGoalButtonFragment.setOnClickListener(new View.OnClickListener(){
